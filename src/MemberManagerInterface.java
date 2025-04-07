2e752c0f7d7e49b10245e061cf8413faa335690c
excuse-rang <excuserang@snu.ac.kr>
2025-04-07 19:38:02 +0900

    Update removeMember method to accept name instead of ID in MemberManagerInterface

--- src/MemberManagerInterface.java
+++ src/MemberManagerInterface.java
@@ -1,6 +1,6 @@
 public interface MemberManagerInterface {  
     void addMember(Member member);
-    void removeMember(String id); // Remove a member by ID
+    void removeMember(String name); // Remove a member by ID
     void listStudents(); // List all members
     void listStaff(); // List all staff members
     
